68047cce93120a9ffec029f933194f0f201489f8
Anthony Lopez <tony.lopez.15@gmail.com>
2018-08-27 15:03:14 -0400

    Commit first

---
+++ clientes/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+
+class Person(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    salary = models.DecimalField(max_digits=5, decimal_places=2)
+    age = models.IntegerField()
+    bio = models.TextField()
+    photo = models.ImageField(upload_to='clients_photos', null=True, blank=True)
+
+    def __str__(self):
+        return self.first_name
+
+
+
