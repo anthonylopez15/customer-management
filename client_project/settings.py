714c5bffec2924445dd93b744fc8393a90020e77
Anthony Lopez <tony.lopez.15@gmail.com>
2018-09-04 15:56:22 -0400

    alterando static files para uso com s3

--- client_project/settings.py
+++ client_project/settings.py
@@ -125,8 +125,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
-STATIC_URL = '/static/'
-
 MEDIA_URL = '/media/'
 
 MEDIA_ROOT = 'media'
@@ -140,3 +138,17 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     'statics',
 ]
+
+
+AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = 'customer-management'
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+AWS_S3_OBJECT_PARAMETERS = {
+    'CacheControl': 'max-age=86400',
+}
+AWS_LOCATION = 'static'
+
+STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
--- clientes/models.py
+++ clientes/models.py
@@ -1,6 +1,13 @@
 from django.db import models
 
 
+class Documento(models.Model):
+    num_doc = models.CharField(max_length=35)
+
+    def __str__(self):
+        return self.num_doc
+
+
 class Person(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
@@ -8,6 +15,7 @@ class Person(models.Model):
     age = models.IntegerField()
     bio = models.TextField()
     photo = models.ImageField(upload_to='clients_photos', null=True, blank=True)
+    doc = models.OneToOneField(Documento, null=True, blank=True, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.first_name
